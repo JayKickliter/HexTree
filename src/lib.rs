b8c6f7e47dbc7d0a7879919adf9b13b9703147fa
Jay Kickliter <jay@kickliter.com>
2022-08-17 16:18:44 -0600

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,82 @@
+pub use h3ron;
+use h3ron::{H3Cell, HasH3Resolution, Index};
+#[cfg(feature = "use-serde")]
+use serde::{Deserialize, Serialize};
+
+/// An HTree is a b(ish)-tree-like structure of hierarchical H3
+/// hexagons, allowing for efficient region lookup.
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "use-serde", derive(Serialize, Deserialize))]
+pub struct HTree {
+    /// First level, and coarsest, H3 resolution of the tree.
+    root_res: u8,
+    nodes: Vec<Node>,
+}
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[cfg_attr(feature = "use-serde", derive(Serialize, Deserialize))]
+struct Node {
+    hex: H3Cell,
+    children: Option<Vec<Node>>,
+}
+
+impl Node {
+    pub fn new(hex: H3Cell) -> Self {
+        Self {
+            hex,
+            children: None,
+        }
+    }
+
+    pub fn insert(&mut self, hex: H3Cell) {
+        assert!(self.hex.resolution() >= hex.resolution());
+        // Are we inserting a hex that covers all possible children?
+        // If so, get rid of the children.
+        if self.hex == hex {
+            self.children = None;
+            return;
+        }
+        if self.children.is_none() {
+            self.children = Some(Vec::with_capacity(1));
+        }
+    }
+
+    pub fn contains(&self, hex: H3Cell) -> bool {
+        unimplemented!()
+    }
+}
+
+impl HTree {
+    /// Create a new HTree with given root resolution.
+    pub fn new(root_res: u8) -> Self {
+        Self {
+            root_res,
+            nodes: Vec::new(),
+        }
+    }
+
+    pub fn insert(&mut self, hex: H3Cell) {
+        assert!(hex.h3_resolution() >= self.root_res);
+        let promoted = hex.get_parent(self.root_res).unwrap();
+        match self.nodes.binary_search_by_key(&promoted, |node| node.hex) {
+            Ok(pos) => {
+                self.nodes[pos].insert(hex);
+            }
+            Err(pos) => {
+                let mut node = Node::new(promoted);
+                node.insert(hex);
+                self.nodes.insert(pos, node);
+            }
+        }
+    }
+
+    pub fn contains(&self, hex: H3Cell) -> bool {
+        assert!(hex.resolution() >= self.root_res);
+        let promoted = hex.get_parent(self.root_res).unwrap();
+        if let Ok(pos) = self.nodes.binary_search_by_key(&promoted, |node| node.hex) {
+            self.nodes[pos].contains(hex)
+        } else {
+            false
+        }
+    }
+}
