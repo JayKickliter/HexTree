4a8feed379929af09dbb336203726ad6a836a43d
Jay Kickliter <jay@kickliter.com>
2022-08-25 16:19:25 -0600

    Add working doc tests

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,45 @@
 //! * **`serde-support`**: support for \[de\]serializing a HexSet via [serde].
 //!
 //! [serde]: https://docs.rs/serde/latest/serde/
+//!
+//! # Usage
+//!
+//! <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1Ty1LhqAipSTl6lsXH7YAjE6kdNmEvCw&ehbc=2E312F" width="640" height="480"></iframe>
+//!
+//! ----
+//!
+//! Let's create a HexSet for Monaco as visualized in the map
+//!
+//! ```
+//! # use hexset::h3ron::Error;
+//! #
+//! # fn main() -> Result<(), Error> {
+//! use geo_types::coord;
+//! use hexset::{h3ron::H3Cell, HexSet};
+//! #
+//! #    use byteorder::{LittleEndian as LE, ReadBytesExt};
+//! #    use hexset::h3ron::FromH3Index;
+//! #    let idx_bytes = include_bytes!("../assets//monaco.res12.h3idx");
+//! #    let rdr = &mut idx_bytes.as_slice();
+//! #    let mut cells = Vec::new();
+//! #    while let Ok(idx) = rdr.read_u64::<LE>() {
+//! #        cells.push(H3Cell::from_h3index(idx));
+//! #    }
+//!
+//! // `cells` is a slice of `H3Cell`s
+//! let monaco: HexSet = cells.iter().collect();
+//!
+//! // You can see in the map above that our set covers Point 1 (green
+//! // check) but not Point 2 (red x), let's test that.
+//! let point_1 = H3Cell::from_coordinate(&coord! {x: 7.42418, y: 43.73631}, 12)?;
+//! let point_2 = H3Cell::from_coordinate(&coord! {x: 7.42855, y: 43.73008}, 12)?;
+//!
+//! assert!(monaco.contains(&point_1));
+//! assert!(!monaco.contains(&point_2));
+//!
+//! #     Ok(())
+//! # }
+//! ```
 
 pub use h3ron;
 use h3ron::{H3Cell, Index};
