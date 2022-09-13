6fa7a7328218d808f1099916836bb5b444f44f29
Jay Kickliter <jay@kickliter.com>
2022-09-13 11:30:21 -0600

    Fill in crate-level docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,44 @@
 #![deny(missing_docs)]
 #![deny(rustdoc::broken_intra_doc_links)]
 
-//! `hextree` provides structures that can represent geographic regions as H3 cells.
+//! hextree provides tree structures that represent geographic regions with H3 cells.
 //!
 //! The primary structures are:
-//! - HexMap: a region to value map.
-//! - HexSet: a region without values.
+//! - HexMap: an H3Cell-to-value map.
+//! - HexSet: an H3Cell set for hit-testing.
 //!
 //! You can think of `HexMap` vs. `HexSet` as [`HashMap`] vs. [`HashSet`].
 //!
-//! [`HashMap`]: https://doc.rust-lang.org/std/collections/struct.HashMap.html
-//! [`HashSet`]: https://doc.rust-lang.org/std/collections/struct.HashSet.html
+//! For the rest of the documentation, we will use hextree to refer to
+//! the general data structure.
 //!
-//! # Features
+//! ## How is this different from `HashMap<H3Cell, V>`?
+//!
+//! The key feature of a hextree is that its keys (H3 cells) are
+//! hierarchical. For instance, if you previously inserted an entry
+//! for a low-res hex, but later query for a higher-res child hex, the
+//! tree returns the value for the lower res hex. Additionally, with
+//! [compaction], trees can automatically coalesce adjacent high-res
+//! hexagons into their parent hex. For every large regions, the
+//! compaction process _can_ continue to lowest resolution cells
+//! (res-0), possibly removing millions of redundant cells from the
+//! tree.
+//!
+//! A hextree's internal structure exactly matches the semantics of an
+//! [H3 cell]. The root of the tree has 122 resolution-0 nodes,
+//! followed by 15 levels of 7-ary nodes. The level of an occupied
+//! node, or leaf node, is the same as its corresponding H3 cell
+//! resolution.
+//!
+//! ## Features
 //!
 //! * **`serde-support`**: support for serialization via [serde].
 //!
+//! [`HashMap`]: https://doc.rust-lang.org/std/collections/struct.HashMap.html
+//! [`HashSet`]: https://doc.rust-lang.org/std/collections/struct.HashSet.html
+//! [H3 cell]: https://h3geo.org/docs/core-library/h3Indexing
 //! [serde]: https://docs.rs/serde/latest/serde
+//! [compaction]: crate::compaction
 
 pub mod compaction;
 mod digits;
