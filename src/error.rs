41f4f648e813f36211278e42cac01524f6554528
Jay Kickliter <jay@kickliter.com>
2023-02-24 16:05:00 -0700

    impl Error and remove dep

--- src/error.rs
+++ src/error.rs
@@ -1,12 +1,19 @@
-use thiserror::Error;
-
 /// Result type for this crate
 pub type Result<T = ()> = std::result::Result<T, Error>;
 
 /// Error type for this crate.
-#[derive(Error, Debug)]
+#[derive(Debug)]
 pub enum Error {
     /// An invalid raw source value was used for an H3 cell.
-    #[error("raw u64 does not a valid H3 index: {0}")]
     Index(u64),
+}
+
+impl std::error::Error for Error {}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Error::Index(bits) => write!(f, "raw u64 is not a valid H3 index: {bits}"),
+        }
+    }
 }
